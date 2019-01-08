526087b565860056efe1a7c55b5d7d08fd4e7b27
Stephan Allen <sallen202009@gmail.com>
2019-01-07 21:51:15 -0500

    Updates

---
+++ precache-manifest.510707babad3851849a7cc4bad804b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9ab9ad9160cb4dbb428",
+    "url": "/cartoon-memory-game/static/js/runtime~main.f9ab9ad9.js"
+  },
+  {
+    "revision": "fc339d459a7eb57adeda",
+    "url": "/cartoon-memory-game/static/js/main.fc339d45.chunk.js"
+  },
+  {
+    "revision": "54d818b3e7605f1c00a9",
+    "url": "/cartoon-memory-game/static/js/1.54d818b3.chunk.js"
+  },
+  {
+    "revision": "fc339d459a7eb57adeda",
+    "url": "/cartoon-memory-game/static/css/main.e7f27dc7.chunk.css"
+  },
+  {
+    "revision": "2f90b99629f2fc8b5f55091ddb9e8032",
+    "url": "/cartoon-memory-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cartoon-memory-game/precache-manifest.510707babad3851849a7cc4bad804b5a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cartoon-memory-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

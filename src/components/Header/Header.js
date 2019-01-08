0602af9aa883585232340224a2f012ac9b426246
Stephan Allen <sallen202009@gmail.com>
2019-01-07 20:26:38 -0500

    first and last commit 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@ function Header(props) {
   return (
     <div>
       <nav className="navbar">
-          <p>Cartoon Memory <i className="fas fa-gamepad fa-lg"></i></p>
+          <p>Cartoon Memories <i className="fas fa-gamepad fa-lg"></i></p>
           <p>Click an image to begin!</p>
           <p>Score: {props.score} | Top Score: {props.topScore}</p>
       </nav>
       <div className='jumbotron header'>
-        <h1 className='text-center'>Lets play Cartoon Memory!</h1>
+        <h1 className='text-center'>Lets play Cartoon Memories!</h1>
         <p className='text-center lead'>Click on an image to earn points, but don't click on any more than once!</p>
       </div>
     </div>
